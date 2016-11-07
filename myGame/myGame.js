0da248298ee58f97a701fba8a0f7ac5f0580c39b
Chris Lim <clim.sh@mysummitps.org>
2016-11-07 20:11:23 +0000

    update

--- myGame/myGame.js
+++ myGame/myGame.js
@@ -13,13 +13,15 @@ game_state.main.prototype = {
         game.load.image('sky', 'assets/sky.png');
         game.load.image('ground', 'assets/platform.png');
         game.load.image('star', 'assets/star.png');
-        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        game.load.spritesheet('dude', 'assets/icon.png', 128, 120);
+        game.load.spritesheet('diamond', 'assets/diamond.png');
 
     },
 
 
     create: function() {
         game.add.sprite(0, 0, 'star');
+        game.add.sprite(0, 0, 'diamond');
         // A simple background for our game
         game.add.sprite(0, 0, 'sky');
         // The platforms group contains the ground and the 2 ledges we can jump on
@@ -38,9 +40,9 @@ game_state.main.prototype = {
         ground.body.immovable = true;
 
         // Now let's create two ledges
-        var ledge = this.platforms.create(0, 250, 'ground');
+        var ledge = this.platforms.create(0, 290, 'ground');
         ledge.body.immovable = true;
-        var ledge = this.platforms.create(400, 150, 'ground');
+        var ledge = this.platforms.create(400, 120, 'ground');
         ledge.body.immovable = true;
 
         // We're going to be using physics, so enable the Arcade Physics system
@@ -53,30 +55,34 @@ game_state.main.prototype = {
         game.physics.arcade.enable(this.player);
 
         // Player physics properties. Give the little guy a slight bounce.
-        this.player.body.bounce.y = 0.999999999;
+        this.player.body.bounce.y = 0.95;
         this.player.body.gravity.y = 250;
         this.player.body.collideWorldBounds = true;
 
         // Our two animations, walking left and right.
-        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+        this.player.animations.add('left', [1, 2, 3, 2], true);
+        this.player.animations.add('right', [7], true);
 
         // Finally some this.stars to collect
         this.stars = game.add.group();
+        this.diamond = game.add.group();
 
         // We will enable physics for any star that is created in this group
         this.stars.enableBody = true;
-
+        this.diamond.enableBody = true;
+        
         // Here we'll create 12 of them evenly space apart
         for (var i = 0; i < 12; i++) {
             // Create a star inside of the 'this.stars' group
             var star = this.stars.create(i * 70, 0, 'star');
-
+            var diamond = this.diamond.create(i * 60, 0, 'diamond');
             // Let gravity do its thing
             star.body.gravity.y = 300;
+            diamond.body.gravity.y = 300;
 
             // This just gives each star a slightly random bounce value
             star.body.bounce.y = 0.7 + Math.random() * 0.2;
+            diamond.body.bounce.y = 0.6 + Math.random() * 0.3;
 
             // The this.score
 
@@ -118,7 +124,7 @@ game_state.main.prototype = {
             // Stand still
             this.player.animations.stop();
 
-            this.player.frame = 4;
+            this.player.frame = 0;
         }
 
         // Allow the this.play to jump if they are touching the ground.
@@ -127,10 +133,11 @@ game_state.main.prototype = {
         }
         // Collide the stars and the platforms
         game.physics.arcade.collide(this.stars, this.platforms);
+        game.physics.arcade.collide(this.diamond, this.platforms);
 
         // Checks to see if the this.player overlaps with any of the this.stars, if he does call the collectStar function
         game.physics.arcade.overlap(this.player, this.stars, this.collectStar, null, this);
-
+        game.physics.arcade.overlap(this.player, this.diamond, this.collectDiamond, null, this);
 
 
     },
@@ -140,10 +147,15 @@ game_state.main.prototype = {
         star.kill();
         this.score += 1;
         this.scoreText.text = "Score:" + this.score;
-
+    
+    },
+    
+    collectDiamond: function(player, diamond) {
+        // Removes the star from the screen
+        diamond.kill();
+        this.score += 2;
+        this.scoreText.text = "Score:" + this.score;
     }
-
-
 };
 
 game.state.add('main', game_state.main);
